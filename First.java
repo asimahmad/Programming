96fbfbe1980ab833ae9ad0207cfa8eb1d39bfb5f
Asim <asim005011997@gmail.com>
2020-07-01 16:32:02 +0530

    First Java

---
+++ First.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Test
+{
+    public static void main(String ar[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("The addition of two numbers: "+(a+b));
+        sc.close();
+    }
+}
