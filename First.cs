55b6c75740ae46d0c195ff2e67608f6cf699bd35
Asim <asim005011997@gmail.com>
2020-07-01 17:09:12 +0530

    C Sharp

---
+++ First.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Test
+{
+    class Demo
+    {
+        int a = 42;
+        int b = 119;
+        int c = a + b;
+        Console.WriteLine(c);
+        Console.ReadKey();
+    }   
+}
